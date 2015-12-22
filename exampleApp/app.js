d85142a75ab1a7f55f1a27c32dc7e23b0393757b
AriFlo <ariflo@arianflores.com>
2015-12-22 13:46:01 -0800

    finished example app with route js

--- exampleApp/app.js
+++ exampleApp/app.js
@@ -1,22 +1,13 @@
 var express = require("express");
 var app = express(); 
+var routes = require('./router/routes')();
 
 
 app.set("view engine", "ejs");
 app.set('views', __dirname + '/views/');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req,res){
-	res.render("pages/index");
-});
-
-app.get('/nightmare', function(req,res){
-	res.render("pages/expressHelp");
-});
-
-app.get('/semantic', function(req,res){
-	res.redirect("/nightmare");
-});
+app.use('/', routes);
 
 app.listen(3000, function(){
 	console.log("Listening on 3000");
